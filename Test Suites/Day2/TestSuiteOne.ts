ea3e738c9ae7d776eedec682f74f9d61a0ea39c3
Raghav-Pal <automtion.devops@gmail.com>
2019-10-29 20:56:23 +0530

    added test suite

---
+++ Test Suites/Day2/TestSuiteOne.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuiteOne</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>d864fcf4-9db8-4026-9eb3-50de7063f6cd</testSuiteGuid>
+   <testCaseLink>
+      <guid>63784240-1953-42ff-b54a-8871f122d369</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Day2/DebuggingDemo</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
